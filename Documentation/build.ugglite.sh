526229d02f3e33c91d03046b06e3d7ce11fce964
fadlyas07 <fadlyardhians@gmail.com>
2020-05-13 16:55:43 +0800

    build-scripts: sync to latest scripts (fadlyas07/Scripts)
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- Documentation/build.ugglite.sh
+++ Documentation/build.ugglite.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Circle CI/CD - Simple kernel build script
-# Copyright (C) 2019 Raphielscape LLC (@raphielscape)
-# Copyright (C) 2019 Dicky Herlambang (@Nicklas373)
-# Copyright (C) 2020 Muhammad Fadlyas (@fadlyas07)
-
+# Copyright (C) 2019, 2020, Raphielscape LLC (@raphielscape)
+# Copyright (C) 2019, 2020, Dicky Herlambang (@Nicklas373)
+# Copyright (C) 2020, Muhammad Fadlyas (@fadlyas07)
+git clone --depth=1 --single-branch https://github.com/kdrag0n/proton-clang
+git clone --depth=1 --single-branch https://github.com/fabianonline/telegram.sh telegram
+git clone --depth=1 --single-branch https://github.com/fadlyas07/anykernel-3
+mkdir $(pwd)/temp
 export ARCH=arm64
 export TEMP=$(pwd)/temp
 export TELEGRAM_ID=$chat_id
@@ -11,50 +14,58 @@ export TELEGRAM_TOKEN=$token
 export pack=$(pwd)/anykernel-3
 export product_name=GreenForce
 export device="Xiaomi Redmi Note 5A"
-export KBUILD_BUILD_HOST=$(git log --format='%cn' -1)
-export KBUILD_BUILD_USER=$(git log --format='%H' -1)
-export parse_branch=$(git rev-parse --abbrev-ref HEAD)
+export KBUILD_BUILD_HOST=$(git log --format='%H' -1)
+export KBUILD_BUILD_USER=$(git log --format='%cn' -1)
 export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 build_start=$(date +"%s")
 
-mkdir $(pwd)/temp
-git clone --depth=1 --single-branch https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5900059 clang
-echo "GCC 4.9.x 20150123 ARCH64 & ARM32 already in /root/ directory"
-git clone --depth=1 --single-branch https://github.com/fabianonline/telegram.sh telegram
-git clone --depth=1 --single-branch https://github.com/fadlyas07/anykernel-3
-
+tg_sendstick() {
+   curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
+ -d sticker="CAACAgUAAxkBAAEYl9pee0jBz-DdWSsy7Rik8lwWE6LARwACmQEAAn1Cwy4FwzpKLPPhXRgE" \
+ -d chat_id="$TELEGRAM_ID"
+}
 TELEGRAM=telegram/telegram
 tg_channelcast() {
     "$TELEGRAM" -c "$TELEGRAM_ID" -H \
-	"$(
-		for POST in "$@"; do
-			echo "$POST"
-		done
-	)"
+ "$(
+  for POST in "$@"; do
+   echo "$POST"
+  done
+ )"
 }
-
-date1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
-make ARCH=arm64 O=out ugglite_defconfig && \
-PATH=$(pwd)/clang/bin:/root/gcc/bin:/root/gcc32/bin:$PATH \
-make -j$(nproc --all) O=out \
-		      ARCH=arm64 \
-		      CC=clang \
-		      CLANG_TRIPLE=aarch64-linux-gnu- \
-		      CROSS_COMPILE=aarch64-linux-android- \
-		      CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee build_kernel.log
+tg_build() {
+export KBUILD_COMPILER_STRING=$($(pwd)/proton-clang/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+PATH=$(pwd)/proton-clang/bin:$PATH \
+make -j$(nproc) O=out \
+                ARCH=arm64 \
+                AR=llvm-ar \
+                CC=clang \
+                CROSS_COMPILE=aarch64-linux-gnu- \
+                CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                NM=llvm-nm \
+                OBJCOPY=llvm-objcopy \
+                OBJDUMP=llvm-objdump \
+                STRIP=llvm-strip
+}
+date=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
+make ARCH=arm64 O=out "ugglite_defconfig" && \
+tg_build 2>&1| tee $(TZ=Asia/Jakarta date +'%A-%H%M-%d%m%y').log
 mv *.log $TEMP
-if [[ ! -f "$kernel_img" ]]; then
-	curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-	tg_channelcast "$product_name $device Build Failed!"
-	exit 1
+if ! [[ -f "$kernel_img" ]]; then
+    build_end=$(date +"%s")
+    build_diff=$(($build_end - $build_start))
+    curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
+    tg_channelcast "<b>$product_name</b> for <b>$device</b> at commit <b>$(git log --pretty=format:'%s' -1)</b> Build errors in $(($build_diff / 60)) minutes and $(($build_diff % 60)) seconds."
+    exit 1
 fi
 curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
 mv $kernel_img $pack/zImage
-cd $pack && zip -r9q $product_name-ugglite-$date1.zip * -x .git README.md LICENCE $(echo *.zip)
+cd $pack && zip -r9q $product_name-ugglite-$date.zip * -x .git README.md LICENCE $(echo *.zip)
 cd ..
 build_end=$(date +"%s")
 build_diff=$(($build_end - $build_start))
 kernel_ver=$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)
 toolchain_ver=$(cat $(pwd)/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
+tg_sendstick
 tg_channelcast "⚠️ <i>Warning: New build is available!</i> working on <b>$parse_branch</b> in <b>Linux $kernel_ver</b> using <b>$toolchain_ver</b> for <b>$device</b> at commit <b>$(git log --pretty=format:'%s' -1)</b>. Build complete in $(($build_diff / 60)) minutes and $(($build_diff % 60)) seconds."
 curl -F document=@$(echo $pack/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
